5a065094450d1249ec0060191691efd73481bec2
Ximena Gonzalez <ximena.glez@gmail.com>
2023-03-14 08:53:08 -0600

    select file pySimpleGUI

--- curriculumClassifierTerra/interface.py
+++ curriculumClassifierTerra/interface.py
@@ -1,18 +1,25 @@
 import PySimpleGUI as sg
+import os.path
 
-sg.theme('DarkAmber')   # Add a touch of color
+sg.theme("DarkTeal2")   # Add a touch of color
+
+#layout de dos columnas
+
+
 # All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [  [sg.T("")],
+            [sg.Text('Choose a CV'), sg.Input()],
+            [sg.FileBrowse(key= "-IN-")],
+            [sg.Button('Submit'), sg.Button('Cancel')] ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Evaluador de CVs', layout, size=(600,150))
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event=="Cancel":
         break
-    print('You entered ', values[0])
+    elif event == "Submit":
+        print(values["-IN-"])
 
 window.close()
