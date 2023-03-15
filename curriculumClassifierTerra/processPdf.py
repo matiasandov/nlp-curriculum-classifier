68031a3d26199ea767f1749e6d6ab798e1936064
matiasandov <a01422885@itesm.mx>
2023-03-14 21:40:58 -0600

    corrected preProcesing and NN

--- curriculumClassifierTerra/processPdf.py
+++ curriculumClassifierTerra/processPdf.py
@@ -8,6 +8,23 @@ import sklearn
 #Documentacion https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
 from sklearn.feature_extraction.text import CountVectorizer
 import pickle
+#Procesaamiento del input
+import re 
+import nltk 
+
+# Stopwords
+nltk.download("stopwords") 
+from nltk.corpus import stopwords 
+
+# Stemming --> get root of the word
+from nltk.stem.porter import PorterStemmer 
+from nltk.stem import SnowballStemmer # best stemmer
+
+
+# Lemmatization --> get context
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.stem.wordnet import WordNetLemmatizer
 
 
 
@@ -35,45 +52,60 @@ def process_response(pathPdf):
 
 def call_model(text):
    
-    stopwords_espanol = stopwords.words('spanish')
-    stopwords_cv = ['correo', 'contacto', 'mail', 'email', 'telefono', 'celular','direccion', 'domicilio', 'perfil']
-    stopwords_espanol = stopwords_espanol + stopwords_cv
-    stopwords_espanol = [unidecode(s) for s in stopwords_espanol]
-    # remove accents from each string in the array
+    
+    #Porcesamiento input
+    eliminarAcentos = lambda str: str.translate(str.maketrans("áàäéèëíìïòóöùúüÀÁÄÈÉËÌÍÏÒÓÖÙÚÜ", "aaaeeeiiiooouuuAAAEEEIIIOOOUUU"))
 
-    #strip_accents: Remove accents and perform other character normalization during the preprocessing step.
-    vectorizer = CountVectorizer(
-        max_features=70,
-        lowercase = True, 
-        strip_accents = 'ascii',
-        analyzer = "word", 
-        stop_words = stopwords_espanol)
-    0
-    data = vectorizer.fit_transform([text])
+   
+
+    # 0. Quitamos acentos:
+    review = eliminarAcentos(text[0])
+    # 1. Quitamos todo lo que no sea una letra:
+    review = re.sub("[^a-zA-Z]", " ", review) 
+    # 2. Todo en minúsculas:
+    review = review.lower()
+    # 2.1 Quitamos espacios multiples y dejamos solo uno:
+    review = re.sub("[\s]+", " ", review)
+
+    # 2.2 Extra: Quitar palabras de menos de 3 letras y espacios al principio y final
+    review = re.sub(r"\W*\b\w{1,3}\b", "", review)
+    review = review.strip()
+
+    # 3. Separamos oraciones para obtener puras palabras
+    review = review.split()
+
+    # 4. Extraer el significado detrás de cada palabra Stemming o Lemmatization y eliminar ruido
+    #stemmer = SnowballStemmer('spanish') # Best stemmer
+    #ps = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+
+    # 4.1 Además queremos quitar palabras irrelevantes como artículos
+    all_stopwords = stopwords.words("spanish") 
+    #all_stopwords.remove("palabra") # --> para excepciones que no queramos incluir
+
+    # 4.2 Unimos el Stemmer/Lematization para cada palabra que no esté dentro de la lista de stopwords
+    #review = [stemmer.stem(word) for word in review if word not in set(all_stopwords)]
+    review = [lemmatizer.lemmatize(word) for word in review if word not in set(all_stopwords)]
+    #review = [word for word in review if word not in set(all_stopwords)]
+    review = [" ".join(review)]
+    
+    
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(review).toarray()
+
 
     
 
-    # Load the saved model weights
-    filename = 'naive_model_weights.pkl'
-    with open(filename, 'rb') as f:
-        saved_clf = pickle.load(f)
-
-    # Instantiate a new MultinomialNB with the saved weights
-    new_clf = MultinomialNB()
-    new_clf.classes_ = saved_clf.classes_
-    new_clf.feature_log_prob_ = saved_clf.feature_log_prob_
-    new_clf.class_log_prior_ = saved_clf.class_log_prior_
-    #Nombre de la prediccion
-    prediccion = new_clf.predict(data)
-    print(prediccion)
-    #prediccion = prediccion[0]
+    pickled_model = pickle.load(open('ann_model_weights.pkl', 'rb'))
+    prediccion = pickled_model.predict(X)
+  
     #Probabilidad numerica
-    probabilidad = new_clf.score(data, prediccion)
-    prediccion = prediccion[0]
+    #probabilidad = new_clf.score(data, prediccion)
+    #prediccion = prediccion[0]
     #probabilidad = max(probabilidad)
     
     print(prediccion)
-    return prediccion, probabilidad
+    return prediccion
 
 
 print(process_response("Bestie.pdf"))
